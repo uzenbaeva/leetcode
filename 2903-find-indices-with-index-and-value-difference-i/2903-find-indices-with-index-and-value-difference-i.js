398aab2a5f43281c8dde203df2a067ed2d832526
uzenbaeva <147222935+uzenbaeva@users.noreply.github.com>
2024-07-13 15:09:39 +0600

    Time: 64 ms (34.21%), Space: 50.4 MB (72.81%) - LeetHub

---
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} indexDifference
+ * @param {number} valueDifference
+ * @return {number[]}
+ */
+var findIndices = function(nums, indexDifference, valueDifference) {
+const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (Math.abs(i - j) >= indexDifference && Math.abs(nums[i] - nums[j]) >= valueDifference) {
+                return [i, j];
+            }
+        }
+    }
+    return [-1, -1];
+}
