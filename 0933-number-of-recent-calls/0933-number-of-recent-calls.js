d3b50686c4d7fb0dd3dad774234be488e9e7a136
uzenbaeva <147222935+uzenbaeva@users.noreply.github.com>
2024-07-13 15:36:39 +0600

    Time: 160 ms (98.07%), Space: 60.4 MB (93.84%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.js
@@ -0,0 +1,13 @@
+class RecentCounter {
+    constructor() {
+        this.requests = [];
+    }
+
+    ping(t) {
+        this.requests.push(t);
+        while (this.requests[0] < t - 3000) {
+            this.requests.shift();
+        }
+        return this.requests.length;
+    }
+}
