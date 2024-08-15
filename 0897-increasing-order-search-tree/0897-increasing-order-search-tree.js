e9a985df7adb714306c7174aaeca42b22bae3ea9
Aku <147222935+uzenbaeva@users.noreply.github.com>
2024-08-15 13:43:10 +0600

    Time: 50 ms (78.95%), Space: 50.4 MB (74.09%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.js
@@ -0,0 +1,21 @@
+var increasingBST = function(root) {
+  if (!root) {
+    return root;
+  }
+  
+  const leftRoot = increasingBST(root.left);
+  root.left = null;
+  root.right = increasingBST(root.right);
+  
+  if (!leftRoot) {
+    return root;
+  }
+  
+  let lastLeftNode = leftRoot;
+  while (lastLeftNode.right) {
+    lastLeftNode = lastLeftNode.right;
+  }
+  
+  lastLeftNode.right = root;
+  return leftRoot;
+};
